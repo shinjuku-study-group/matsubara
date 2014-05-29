18dae9cc4ecf853ab08da5072761bf72e5a54598
simezi <y74RGeis>
2014-05-30 08:53:28 +0900

    refactor

--- study/src/main/scala/monster.scala
+++ study/src/main/scala/monster.scala
@@ -3,16 +3,15 @@
  */
 object monster {
   def main(args: Array[String]) {
-    println(func(1000))
+    println(func(10))
   }
 
   def func(mons: Int): Double = {
-    import Math.pow
     /*モンスターが奇数の場合生存不可*/
     if (mons % 2 == 1) 0.0
     /*モンスターがいなくなれば生存*/
     else if (mons == 0) 1.0
     /*モンスターが二匹いなくなるパターンのみを追求*/
-    else pow(mons, 2) / pow(mons + 1, 2) * func(mons - 2)
+    else (mons - 1) / (mons + 1).asInstanceOf[Double] * func(mons - 2)
   }
 }
