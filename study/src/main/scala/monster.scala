5c157d1f1f5dc3609021795ee35252c6760354b3
simezi <y74RGeis>
2014-05-30 07:25:59 +0900

    idea commit

--- study/src/main/scala/monster.scala
+++ study/src/main/scala/monster.scala
@@ -3,16 +3,15 @@
  */
 object monster {
   def main(args: Array[String]) {
-    println(func(6, 5))
+    println(func(10))
   }
 
-  def func(mons: Int, rabbit: Int): Double = {
-    println(mons)
-    //モンスターが奇数の場合生存不可
-    if (mons % 2 == 1) {
-      return 0.0
-    }
-    if (mons == 0) 1.0
-    else ((mons * mons) / Math.pow(1 + mons, 2)) * func(mons - 2, rabbit)
+  def func(mons: Int): Double = {
+    /*モンスターが奇数の場合生存不可*/
+    if (mons % 2 == 1) 0.0
+    /*モンスターがいなくなれば生存*/
+    else if (mons == 0) 1.0
+    /*モンスターが二匹いなくなるパターンのみを追求*/
+    else ((mons * mons) / Math.pow(1 + mons, 2)) * func(mons - 2)
   }
 }
