76112ab49a281d069068c0df897a67d9a573a8b3
simezi <yuusuke.matsubara@nssys.com>
2014-05-31 18:43:31 +0900

    twitterの始まり・・・？

--- study/src/main/scala/TwitterMain.scala
+++ study/src/main/scala/TwitterMain.scala
@@ -8,25 +8,24 @@ import twitter4j.Status
 
 object TwitterMain {
   def main(args: Array[String]) {
-    val cb = new ConfigurationBuilder();
+    val cb = new ConfigurationBuilder()
     cb.setDebugEnabled(false)
       .setHttpProxyHost("proxy")
       .setHttpProxyPort(8080)
       .setOAuthConsumerKey("Xz7SZlSoCbGW7exm9zQYoUPxF")
       .setOAuthConsumerSecret("TQuHoTZP8s3kg7CvGbAoijdgs1xysqA9w2fA0vBjIzgyKZbvhC")
       .setOAuthAccessToken("89462095-dZkiMHvEIYkD4RmutK1PnUcSxxxELk5iMODvflxZy")
-      .setOAuthAccessTokenSecret("RzxXiBuA2Fvd8PcornQsO4LjgputqRNJHYfoYH9YhL0SV");
-    val tf = new TwitterFactory(cb.build());
-    val twitter = tf.getInstance();
+      .setOAuthAccessTokenSecret("RzxXiBuA2Fvd8PcornQsO4LjgputqRNJHYfoYH9YhL0SV")
+    val tf = new TwitterFactory(cb.build())
+    val twitter = tf.getInstance()
     println(twitter.getAccountSettings)
 
 
     import collection.JavaConversions._
-    val statuses = twitter.getHomeTimeline()
+    val statuses = twitter.getHomeTimeline
     println("Showing home timeline.")
     for (status: Status <- statuses) {
-      println(status.getUser().getName() + ":" +
-        status.getText());
+      println(status.getUser.getName + ":" + status.getText)
     }
   }
 }
